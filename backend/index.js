a90a57cd87217a5921ca2124085964b68ee4077c
Manisha Sahoo <manishasahoo2343@gmail.com>
2026-01-05 00:09:31 +0530

    Company Controller and router created and checked in postman successfully

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+
 dotenv.config({});
 
 const app = express();
@@ -22,6 +24,8 @@ const PORT = process.env.PORT || 3000;
 
 //api's
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+
 //"http://localhost:8000/api/v1/user/register"
 
 app.listen(PORT, ()=>{
