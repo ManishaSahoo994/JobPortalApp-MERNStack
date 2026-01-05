afa7eae2ac3a9e929c38d1da910d0e292d734bfe
Manisha Sahoo <manishasahoo2343@gmail.com>
2026-01-06 01:15:10 +0530

    job controller and router created and checked in postman successfully

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 
 dotenv.config({});
 
@@ -25,10 +26,11 @@ const PORT = process.env.PORT || 3000;
 //api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
 
 //"http://localhost:8000/api/v1/user/register"
 
 app.listen(PORT, ()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
