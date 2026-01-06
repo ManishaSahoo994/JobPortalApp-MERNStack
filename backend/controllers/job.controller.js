c087a1356607e8e064654d404df53872824e2c07
Manisha Sahoo <manishasahoo2343@gmail.com>
2026-01-06 08:53:05 +0530

    final checked in postman

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -14,7 +14,7 @@ export const postJob = async (req, res) => {
         const job = await Job.create({
             title,
             description,
-            requirements: requirements.split(","),
+            requirements: requirements,
             salary:Number(salary),
             location,
             jobType,
@@ -42,7 +42,9 @@ export const getAllJobs = async(req, res) => {
                 {description:{$regex:keyword, $options:"i"}},
             ]
         };
-        const jobs = await Job.find(query);
+        const jobs = await Job.find(query).populate({
+            path:"company"
+        }).sort({createdAt:-1});
         if(!jobs){
             return res.status(404).json({
                 message:"Jobs not found.",
