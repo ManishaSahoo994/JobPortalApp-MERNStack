2bec1f84fc1d59736fc244380fae826a1649b0e5
Manisha Sahoo <manishasahoo2343@gmail.com>
2026-01-08 16:59:22 +0530

    application route Checked in postman successfully

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -41,9 +41,10 @@ const jobSchema = new mongoose.Schema({
     ref:'User',
     required:true
     },
-    applications:{
+    applications:[{
     type:mongoose.Schema.Types.ObjectId,
-    ref:'Application'
+    ref:'Application',
     },
+],
 },{timestamps:true});
 export const Job = mongoose.model("Job", jobSchema);
